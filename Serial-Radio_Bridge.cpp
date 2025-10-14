8d2806f1dee6381687966d495e14c95f4e51b874
Jarno Piironen <webronen.github@gmail.com>
2025-10-14 23:41:27 +0300

    Removed unnecessary function.

--- Serial-Radio_Bridge.cpp
+++ Serial-Radio_Bridge.cpp
@@ -3,20 +3,19 @@
 
 #define MBPS_TO_BPS(Mbps) ((Mbps) * 1000000UL)
 
-typedef struct __attribute__((aligned(1), packed))
+typedef struct __attribute__((packet, aligned(1)))
 {
   uint8_t node;
   uint8_t zone;
   uint8_t type;
   uint8_t data[252];
-} DataPacket;
+} Rcu;
 
-static_assert(sizeof(DataPacket) == 255, "DataPacket struct must be 255 bytes");
+static_assert(sizeof(Rcu) == 255, "DataPacket struct must be 255 bytes");
 
-volatile static DataPacket received_packet;
-static DataPacket transmit_packet;
+volatile static Rcu received_packet;
+static Rcu transmit_packet;
 
-static inline void radioInit(void);
 static inline void send_radio_packet(void);
 
 void setup(void)
@@ -29,25 +28,20 @@ void setup(void)
   NRF_RADIO->PACKETPTR = (uint32_t)&received_packet;
   NRF_RADIO->TXPOWER = RADIO_TXPOWER_TXPOWER_Pos8dBm;
 
-  NRF_RADIO->PCNF1 = (sizeof(DataPacket) << RADIO_PCNF1_MAXLEN_Pos) |          // Maximum length of packet payload
-                     (sizeof(DataPacket) << RADIO_PCNF1_STATLEN_Pos) |         // Static length in number of bytes
-                     (2 << RADIO_PCNF1_BALEN_Pos) |                            // Base address length in number of bytes
-                     (RADIO_PCNF1_WHITEEN_Enabled << RADIO_PCNF1_WHITEEN_Pos); // Enable packet whitening
+  NRF_RADIO->PCNF1 = (sizeof(Rcu) << RADIO_PCNF1_MAXLEN_Pos) | (sizeof(Rcu) << RADIO_PCNF1_STATLEN_Pos) | (2 << RADIO_PCNF1_BALEN_Pos) | (RADIO_PCNF1_WHITEEN_Enabled << RADIO_PCNF1_WHITEEN_Pos);
 
   NRF_RADIO->BASE0 = 0x0000BABE;
   NRF_RADIO->PREFIX0 = 0x41 << RADIO_PREFIX0_AP0_Pos;
   NRF_RADIO->RXADDRESSES = RADIO_RXADDRESSES_ADDR0_Msk;
 
-  NRF_RADIO->CRCCNF = (RADIO_CRCCNF_LEN_Two << RADIO_CRCCNF_LEN_Pos) |           // CRC length is two bytes and CRC calculation is enabled
-                      (RADIO_CRCCNF_SKIPADDR_Skip << RADIO_CRCCNF_SKIPADDR_Pos); // CRC calculation does not include address field
+  NRF_RADIO->CRCCNF = (RADIO_CRCCNF_LEN_Two << RADIO_CRCCNF_LEN_Pos) | (RADIO_CRCCNF_SKIPADDR_Skip << RADIO_CRCCNF_SKIPADDR_Pos);
 
   NRF_RADIO->CRCPOLY = 0x0000AAAA;
   NRF_RADIO->CRCINIT = 0x12345678;
 
   NRF_RADIO->DATAWHITEIV = 0x55;
 
-  NRF_RADIO->MODECNF0 = (RADIO_MODECNF0_DTX_B0 << RADIO_MODECNF0_DTX_Pos) | // Transmit 0 when idle
-                        (RADIO_MODECNF0_RU_Fast << RADIO_MODECNF0_RU_Pos);  // Fast ramp-up
+  NRF_RADIO->MODECNF0 = (RADIO_MODECNF0_DTX_B0 << RADIO_MODECNF0_DTX_Pos) | (RADIO_MODECNF0_RU_Fast << RADIO_MODECNF0_RU_Pos);
 
   NRF_RADIO->TASKS_RXEN = 1;
 
@@ -62,13 +56,13 @@ void loop(void)
   {
     NRF_RADIO->EVENTS_CRCOK = 0;
 
-    tud_cdc_n_write(0, (uint8_t *)&received_packet, sizeof(DataPacket));
+    tud_cdc_n_write(0, (uint8_t *)&received_packet, sizeof(Rcu));
     tud_cdc_n_write_flush(0);
   }
 
-  if (tud_cdc_n_available(0) >= sizeof(DataPacket))
+  if (tud_cdc_n_available(0) >= sizeof(Rcu))
   {
-    tud_cdc_n_read(0, (uint8_t *)&transmit_packet, sizeof(DataPacket));
+    tud_cdc_n_read(0, (uint8_t *)&transmit_packet, sizeof(Rcu));
     send_radio_packet();
   }
 }
