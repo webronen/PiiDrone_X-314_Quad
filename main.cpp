4298007a1977e50a2e8a3f4f91ba181dbdd4ea70
Jarno Piironen <webronen.github@gmail.com>
2025-10-17 19:42:08 +0300

    Only update thrust if no power-fail warning is active

--- main.cpp
+++ main.cpp
@@ -342,15 +342,12 @@ static inline void handle_setpoint_update(void)
 
 static inline void handle_thrust_update(void)
 {
-  // Only update thrust if no power-fail warning is active
-  if (!FCU_IS_POFWARN(fcu.status))
-  {
-    uint16_t thrust;
-    memcpy(&thrust, (const void *)&received_packet.data[0], sizeof(thrust));
+  uint16_t thrust;
+  memcpy(&thrust, (const void *)&received_packet.data[0], sizeof(thrust));
 
-    fcu.thrust = constrain(thrust, THRUST_MIN, THRUST_MAX);
-    (fcu.thrust > THRUST_MIN) ? FCU_SET_ACTIVE(fcu.status) : FCU_CLEAR_ACTIVE(fcu.status);
-  }
+  // Only update thrust if no power-fail warning is active.
+  fcu.thrust = FCU_IS_POFWARN(fcu.status) ? fcu.thrust : constrain(thrust, THRUST_MIN, THRUST_MAX);
+  (fcu.thrust > THRUST_MIN) ? FCU_SET_ACTIVE(fcu.status) : FCU_CLEAR_ACTIVE(fcu.status);
 }
 
 static inline void flash_read(void)
