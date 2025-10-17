8ff1480544ed9020ecdb73fb2a5b7511db4325c0
Jarno Piironen <webronen.github@gmail.com>
2025-10-17 21:25:12 +0300

    EDIT: Only update thrust if no power-fail warning is active.

--- main.cpp
+++ main.cpp
@@ -127,7 +127,7 @@ void setup(void)
   vl53l4cx.VL53L4CX_SetUserROI(&roi);
   vl53l4cx.VL53L4CX_StartMeasurement();
 
-  // Load saved FCU settings from flash
+  // Load saved PID gains from flash
   flash_read();
 }
 
@@ -366,9 +366,8 @@ static inline void handle_thrust_update(void)
 {
   uint16_t thrust;
   memcpy(&thrust, (const void *)&received_packet.data[0], sizeof(thrust));
-
   // Only update thrust if no power-fail warning is active.
-  fcu.thrust = FCU_IS_POFWARN(fcu.status) ? fcu.thrust : constrain(thrust, THRUST_MIN, THRUST_MAX);
+  fcu.thrust = constrain(FCU_IS_POFWARN(fcu.status) ? fcu.thrust : thrust, THRUST_MIN, THRUST_MAX);
   (fcu.thrust > THRUST_MIN) ? FCU_SET_ACTIVE(fcu.status) : FCU_CLEAR_ACTIVE(fcu.status);
 }
 
