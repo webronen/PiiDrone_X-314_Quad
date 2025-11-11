dfb6a2127f8e68ff22d1a905817254c2907741c7
Jarno Piironen <webronen.github@gmail.com>
2025-11-11 15:30:49 +0200

    The serial-to-radio bridge for drone control

--- serial-radio-bridge.cpp
+++ serial-radio-bridge.cpp
@@ -1,91 +1,96 @@
-// #include <nrf.h>
-// #include <Adafruit_TinyUSB.h>
+#include <nrf.h>
+#include <Adafruit_TinyUSB.h>
 
-// #define MBPS_TO_BPS(Mbps) ((Mbps) * 1000000UL)
+#define MBPS_TO_BPS(Mbps) ((Mbps) * 1000000UL)
 
-// typedef struct __attribute__((packet, aligned(1)))
-// {
-//   uint8_t node;
-//   uint8_t zone;
-//   uint8_t type;
-//   uint8_t data[252];
-// } Rcu;
+typedef struct __attribute__((packet, aligned(1)))
+{
+  uint8_t node;
+  uint8_t zone;
+  uint8_t type;
+  uint8_t data[252];
+} Rcu;
 
-// static_assert(sizeof(Rcu) == 255, "Rcu struct must be 255 bytes");
+static_assert(sizeof(Rcu) == 255, "Rcu struct must be 255 bytes");
 
-// volatile static Rcu received_packet;
-// static Rcu transmit_packet;
+volatile static Rcu received_packet;
+static Rcu transmit_packet;
 
-// static inline void send_radio_packet(void);
+static inline void send_radio_packet(void);
 
-// void setup(void)
-// {
-//   NRF_CLOCK->TASKS_HFCLKSTART = 1;
-//   while (!NRF_CLOCK->EVENTS_HFCLKSTARTED);
+void setup(void)
+{
+  NRF_CLOCK->TASKS_HFCLKSTART = 1;
+  while (!NRF_CLOCK->EVENTS_HFCLKSTARTED)
+    ;
 
-//   NRF_RADIO->SHORTS = (RADIO_SHORTS_READY_START_Msk | RADIO_SHORTS_END_START_Msk);
-//   NRF_RADIO->PACKETPTR = (uint32_t)&received_packet;
-//   NRF_RADIO->TXPOWER = RADIO_TXPOWER_TXPOWER_Pos8dBm;
+  NRF_RADIO->SHORTS = (RADIO_SHORTS_READY_START_Msk | RADIO_SHORTS_END_START_Msk);
+  NRF_RADIO->PACKETPTR = (uint32_t)&received_packet;
+  NRF_RADIO->TXPOWER = RADIO_TXPOWER_TXPOWER_Pos8dBm;
 
-//   NRF_RADIO->PCNF1 = (sizeof(Rcu) << RADIO_PCNF1_MAXLEN_Pos) |
-//                      (sizeof(Rcu) << RADIO_PCNF1_STATLEN_Pos) |
-//                      (2 << RADIO_PCNF1_BALEN_Pos) |
-//                      (RADIO_PCNF1_WHITEEN_Enabled << RADIO_PCNF1_WHITEEN_Pos);
+  NRF_RADIO->PCNF1 = (sizeof(Rcu) << RADIO_PCNF1_MAXLEN_Pos) |
+                     (sizeof(Rcu) << RADIO_PCNF1_STATLEN_Pos) |
+                     (2 << RADIO_PCNF1_BALEN_Pos) |
+                     (RADIO_PCNF1_WHITEEN_Enabled << RADIO_PCNF1_WHITEEN_Pos);
 
-//   NRF_RADIO->BASE0 = 0x0000BABE;
-//   NRF_RADIO->PREFIX0 = 0x41 << RADIO_PREFIX0_AP0_Pos;
-//   NRF_RADIO->RXADDRESSES = RADIO_RXADDRESSES_ADDR0_Msk;
+  NRF_RADIO->BASE0 = 0x0000BABE;
+  NRF_RADIO->PREFIX0 = 0x41 << RADIO_PREFIX0_AP0_Pos;
+  NRF_RADIO->RXADDRESSES = RADIO_RXADDRESSES_ADDR0_Msk;
 
-//   NRF_RADIO->CRCCNF = (RADIO_CRCCNF_LEN_Two << RADIO_CRCCNF_LEN_Pos) |
-//                       (RADIO_CRCCNF_SKIPADDR_Skip << RADIO_CRCCNF_SKIPADDR_Pos);
+  NRF_RADIO->CRCCNF = (RADIO_CRCCNF_LEN_Two << RADIO_CRCCNF_LEN_Pos) |
+                      (RADIO_CRCCNF_SKIPADDR_Skip << RADIO_CRCCNF_SKIPADDR_Pos);
 
-//   NRF_RADIO->CRCPOLY = 0x0000AAAA;
-//   NRF_RADIO->CRCINIT = 0x12345678;
+  NRF_RADIO->CRCPOLY = 0x0000AAAA;
+  NRF_RADIO->CRCINIT = 0x12345678;
 
-//   NRF_RADIO->DATAWHITEIV = 0x55;
+  NRF_RADIO->DATAWHITEIV = 0x55;
 
-//   NRF_RADIO->MODECNF0 = (RADIO_MODECNF0_DTX_B0 << RADIO_MODECNF0_DTX_Pos) |
-//                         (RADIO_MODECNF0_RU_Fast << RADIO_MODECNF0_RU_Pos);
+  NRF_RADIO->MODECNF0 = (RADIO_MODECNF0_DTX_B0 << RADIO_MODECNF0_DTX_Pos) |
+                        (RADIO_MODECNF0_RU_Fast << RADIO_MODECNF0_RU_Pos);
 
-//   NRF_RADIO->TASKS_RXEN = 1;
+  NRF_RADIO->TASKS_RXEN = 1;
 
-//   Serial.begin(MBPS_TO_BPS(1));
-//   while (!Serial);
-// }
+  Serial.begin(MBPS_TO_BPS(1));
+  while (!Serial)
+    ;
+}
 
-// void loop(void)
-// {
-//   if (NRF_RADIO->EVENTS_CRCOK)
-//   {
-//     NRF_RADIO->EVENTS_CRCOK = 0;
+void loop(void)
+{
+  if (NRF_RADIO->EVENTS_CRCOK)
+  {
+    NRF_RADIO->EVENTS_CRCOK = 0;
 
-//     tud_cdc_n_write(0, (uint8_t *)&received_packet, sizeof(Rcu));
-//     tud_cdc_n_write_flush(0);
-//   }
+    tud_cdc_n_write(0, (uint8_t *)&received_packet, sizeof(Rcu));
+    tud_cdc_n_write_flush(0);
+  }
 
-//   if (tud_cdc_n_available(0) >= sizeof(Rcu))
-//   {
-//     tud_cdc_n_read(0, (uint8_t *)&transmit_packet, sizeof(Rcu));
-//     send_radio_packet();
-//   }
-// }
+  if (tud_cdc_n_available(0) >= sizeof(Rcu))
+  {
+    tud_cdc_n_read(0, (uint8_t *)&transmit_packet, sizeof(Rcu));
+    send_radio_packet();
+  }
+}
 
-// static inline void send_radio_packet(void)
-// {
-//   while (!NRF_RADIO->EVENTS_END);
+static inline void send_radio_packet(void)
+{
+  while (!NRF_RADIO->EVENTS_END)
+    ;
 
-//   NRF_RADIO->EVENTS_END = 0;
-//   NRF_RADIO->TASKS_DISABLE = 1;
+  NRF_RADIO->EVENTS_END = 0;
+  NRF_RADIO->TASKS_DISABLE = 1;
 
-//   while (NRF_RADIO->STATE);
+  while (NRF_RADIO->STATE)
+    ;
 
-//   NRF_RADIO->PACKETPTR = (uint32_t)&transmit_packet;
-//   NRF_RADIO->SHORTS = RADIO_SHORTS_READY_START_Msk | RADIO_SHORTS_END_DISABLE_Msk;
-//   NRF_RADIO->TASKS_TXEN = 1;
+  NRF_RADIO->PACKETPTR = (uint32_t)&transmit_packet;
+  NRF_RADIO->SHORTS = RADIO_SHORTS_READY_START_Msk | RADIO_SHORTS_END_DISABLE_Msk;
+  NRF_RADIO->TASKS_TXEN = 1;
 
-//   while (NRF_RADIO->STATE);
+  while (NRF_RADIO->STATE)
+    ;
 
-//   NRF_RADIO->PACKETPTR = (uint32_t)&received_packet;
-//   NRF_RADIO->SHORTS = RADIO_SHORTS_READY_START_Msk | RADIO_SHORTS_END_START_Msk;
-//   NRF_RADIO->TASKS_RXEN = 1;
-// }
+  NRF_RADIO->PACKETPTR = (uint32_t)&received_packet;
+  NRF_RADIO->SHORTS = RADIO_SHORTS_READY_START_Msk | RADIO_SHORTS_END_START_Msk;
+  NRF_RADIO->TASKS_RXEN = 1;
+}
