584e1a2e2ba5b9ceacc067a8d62b676e2fb56f20
Jarno Piironen <webronen.github@gmail.com>
2025-11-13 19:10:34 +0200

    PID Autotune via Relay Feedback on Drone Bench

--- main.cpp
+++ main.cpp
@@ -162,16 +162,22 @@ static inline void task_fcu_update(void)
 
   if (auto_tune.is_running && !auto_tune.is_at_hover)
   {
-    auto_tune.is_at_hover = pid_thrust_ramp(PID_THRUST_RAMP_MAX, PID_THRUST_RAMP_S);
+    if (pid_thrust_ramp(PID_THRUST_RAMP_MAX, PID_THRUST_RAMP_S))
+    {
+      auto_tune.is_at_hover = true;
+    }
   }
   else if (auto_tune.is_running && auto_tune.is_at_hover)
   {
     static float *const error_ptr[3] = {&error.x, &error.y, &error.z};
     const float current_error = *error_ptr[auto_tune.tuning_axis];
 
-    if (pid_tune_step(auto_tune.tuning_axis, current_error) && ++auto_tune.tuning_axis == 2)
+    if (pid_tune_step(auto_tune.tuning_axis, current_error))
     {
-      auto_tune.is_running = false;
+      if (++auto_tune.tuning_axis == 2)
+      {
+        auto_tune.is_running = false;
+      }
     }
   }
   else if (!auto_tune.is_running && auto_tune.is_at_hover)
@@ -378,12 +384,12 @@ static inline bool pid_thrust_ramp(const float to_thrust, const float in_time_s)
   static uint32_t last_time_us = 0;
 
   NRF_TIMER0->TASKS_CAPTURE[1] = 1;
-  const uint32_t current_time = NRF_TIMER0->CC[1];
+  const uint32_t loop_time_us = NRF_TIMER0->CC[1];
 
   if (!last_time_us)
-    last_time_us = current_time;
+    last_time_us = loop_time_us;
 
-  const uint32_t elapsed_time_us = (current_time - last_time_us);
+  const uint32_t elapsed_time_us = (loop_time_us - last_time_us);
   float x = ((float)elapsed_time_us * S_TO_US_INV(in_time_s));
   x = constrain(x, 0.0f, 1.0f);
 
@@ -414,11 +420,10 @@ static inline bool pid_tune_step(const uint8_t axis, const float err)
   {
     tune[axis].setpoint = PID_AUTOTUNE_AMPLITUDE * DEG_TO_RAD;
     tune[axis].last_change = now + HZ_TO_US(PID_AUTOTUNE_FREQUENCY);
-    tune[axis].last_adj = now + HZ_TO_US(PID_AUTOTUNE_FREQUENCY);
+    tune[axis].last_adj = tune[axis].last_change;
     tune[axis].crosses = 0;
     tune[axis].first_cross = 0;
     tune[axis].active = true;
-
     tune[axis].error_bias = err;
 
     fcu.pid_setpoint[axis] = tune[axis].setpoint;
@@ -440,16 +445,13 @@ static inline bool pid_tune_step(const uint8_t axis, const float err)
   const float hysteresis = __builtin_fabsf(tune[axis].setpoint) * PID_AUTOTUNE_HYSTERESIS;
   const float corrected_err = err - tune[axis].error_bias;
 
-  const bool crossed_positive = (last_err[axis] <= hysteresis) && (corrected_err > hysteresis);
-  const bool crossed_negative = (last_err[axis] >= -hysteresis) && (corrected_err < -hysteresis);
-  const bool crossed_zero = crossed_positive || crossed_negative;
+  const bool crossed_zero = ((last_err[axis] <= hysteresis) && (corrected_err > hysteresis)) ||
+                            ((last_err[axis] >= -hysteresis) && (corrected_err < -hysteresis));
 
   if (crossed_zero)
   {
     if (++tune[axis].crosses == 1)
-    {
       tune[axis].first_cross = now;
-    }
 
     if (tune[axis].crosses >= PID_AUTOTUNE_CROSSES)
     {
@@ -474,9 +476,7 @@ static inline bool pid_tune_step(const uint8_t axis, const float err)
 
     if (fcu.pid_gain[axis][0] > PID_GAIN_MAX)
     {
-      fcu.pid_gain[axis][0] = 0.0f;
-      fcu.pid_gain[axis][1] = 0.0f;
-      fcu.pid_gain[axis][2] = 0.0f;
+      fcu.pid_gain[axis][0] = fcu.pid_gain[axis][1] = fcu.pid_gain[axis][2] = 0.0f;
       fcu.pid_setpoint[axis] = 0.0f;
       tune[axis].active = false;
       return true;
